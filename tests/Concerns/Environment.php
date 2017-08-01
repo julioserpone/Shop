7576b72f5ad7f21bc847f4577a9c9f65d1b7e9bf
gocanto <gustavoocanto@gmail.com>
2017-08-01 15:22:41 -0500

    delete mysql pass on tests

--- tests/Concerns/Environment.php
+++ tests/Concerns/Environment.php
@@ -112,7 +112,7 @@ trait Environment
             'port' => '3306',
             'database' => self::TESTING_DB,
             'username' => 'root',
-            'password' => 'secret',
+            'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
